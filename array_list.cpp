3b2bad33130a808f1ed2a04371e8f8d7fafbb8fe
Henrik Nicolay Topnes Finsberg <henriknf@simula.no>
2023-10-04 08:59:41 +0200

    Add array_list.cpp

---
+++ array_list.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <stdexcept>
+
+class ArrayList
+{
+  private:
+    // Array containing the acutaul data in the list
+    int *_data;
+    // Capacity of the array
+    int _capacity = 1;
+    // Size of the array
+    int _size = 0;
+
+  public:
+    // Default constructor
+    ArrayList()
+    {
+        _data = new int[_capacity];
+    }
+
+    // Destructor
+    ~ArrayList()
+    {
+        delete[] _data;
+    }
+
+    // Length of array
+    int length()
+    {
+        return _size;
+    }
+
+    /**
+     * @brief Get value at a given index.
+     * Throws a range error in index if out of bounds
+     *
+     * @param index The index
+     * @return int The value at that index
+     */
+    int get(int index)
+    {
+        if ((index < 0) || (index >= _size))
+        {
+            throw std::range_error("Index is out of bounds");
+        }
+        return _data[index];
+    }
+
+    /**
+     * @brief Prints the array
+     *
+     */
+    void print()
+    {
+        std::cout << "ArrayList([";
+        for (int i = 0; i < _size - 1; i++)
+        {
+            std::cout << _data[i] << ", ";
+        }
+        std::cout << _data[_size - 1] << "])\n";
+    }
+
+    /**
+     * @brief Get a reference to the value at a given index.
+     * Throws a range error in index if out of bounds
+     *
+     * @param index The index
+     * @return int The value at that index
+     */
+    int &operator[](int index)
+    {
+        if ((index < 0) || (index >= _size))
+        {
+            throw std::range_error("Index is out of bounds");
+        }
+        return _data[index];
+    }
+};
