9a4bd3400244b9601703016e4e01d11c674a543a
kjes4pres <stangelandkjersti@gmail.com>
2023-10-23 12:38:55 +0200

    Checkpoint before starting task 4b).

--- linked_array_list.cpp
+++ linked_array_list.cpp
@@ -18,9 +18,41 @@ struct ArrayListNode
     }
 };
 
+class LinkedArrayList
+{
+    LinkedArrayList()
+    {
+    }
+
+    ~LinkedArrayList()
+    {
+    }
+
+    void append(std::vector<int>)
+    {
+    }
+
+    void append(std::vector<int>)
+    {
+    }
+
+    std::unique_ptr<ArrayList> &operator[](int)
+    {
+    }
+
+
+};
+
 int main()
 {
     ArrayListNode node({1, 2, 3}, nullptr, nullptr);
     node.value->print();
+    // LinkedArrayList lal{}; // Create an empty LinkedArrayList
+    // lal.append({1, 2});    // Append first list
+    // lal.append({4, 5, 6}); // Append a second list
+    // lal.print();           // Print the entire LinkedArrayList
+    // lal[0]->append(42);    // Append the number 42 to the first ArrayList
+    // lal.print();           // Print the entire LinkedArrayList again
+
     return 0;
 }
